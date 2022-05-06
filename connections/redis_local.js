b17bcc8e0880b97c70efa4fd2a98263a7ff8dff6
Tushar S Agrawal <tusharsagrawal16@gmail.com>
2022-05-06 15:24:00 +0530

    redis_ur

--- connections/redis_local.js
+++ connections/redis_local.js
@@ -3,9 +3,7 @@ const redis = require("redis");
 require("dotenv").config();
 
 const client = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-  password: process.env.REDIS_PASSWORD,
+  url: process.env.REDIS_URL,
 });
 
 client.on("error", (err) => console.log("Redis Client Error", err));
